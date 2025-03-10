79a888d46e02edb26fb64f943cfb4f108c45e5cb
alex <alex.bissessur@lasentinelle.mu>
2025-03-10 14:17:24 +0400

    CFP description should be text not string

--- database/migrations/2025_02_19_104138_create_c_f_p_s_table.php
+++ database/migrations/2025_02_19_104138_create_c_f_p_s_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('c_f_p_s', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
             $table->string('name');
             $table->string('phone');
             $table->string('email');
