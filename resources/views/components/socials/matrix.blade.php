5c9219911873b89935533bd256ceed5b50d3b440
Vina D <vinadabydoyal@gmail.com>
2025-10-14 14:53:37 +0400

    Add Matrix link to socials (Hacktoberfest)
    Added a new Matrix community link for Cloud Native Mauritius:
    https://matrix.to/#/#cloudnativemu:matrix.meetup.mu

---
+++ resources/views/components/socials/matrix.blade.php
@@ -0,0 +1,3 @@
+<a href="https://matrix.to/#/#cloudnativemu:matrix.meetup.mu" target="_blank" rel="noopener">
+    <img src="/images/matrix-icon.png" alt="Matrix" style="width:24px;height:24px;"> Matrix
+</a>
